c97b4aa6dd05883c66877674748b425ff95675f5
Joseph <joseph.kal@outlook.com>
2019-07-23 22:26:01 -0300

    first commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import { signInRequest } from '~/store/modules/auth/actions';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid e-mail')
+    .required('E-mail is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+export default function SignIn() {
+  const dispatch = useDispatch();
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">Access</button>
+        <Link to="/register">Crate a free account</Link>
+      </Form>
+    </>
+  );
+}
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,24 @@
+import { takeLatest, all, call, put } from 'redux-saga/effects';
+import history from '~/services/history';
+import api from '~/services/api';
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('User is not provider');
+    return;
+  }
+  yield put(signInSuccess(token, user));
+
+  history.pushState('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
