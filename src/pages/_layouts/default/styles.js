24124dcac6421fafa3deacea99cd01b5a0a68d53
Joseph <joseph.kal@outlook.com>
2019-07-27 01:29:23 -0300

    added appointments feature

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background-image: linear-gradient(90deg, #ce9ffc 10%, #7367f0 100%);
+  background-image: linear-gradient(45deg, #ce9ffc 10%, #7367f0 100%);
 `;
